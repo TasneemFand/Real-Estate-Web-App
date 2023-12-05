c9f2b1a1aa8d638f9ce8e18b8d2b2a4486939370
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-12-05 15:58:58 +0300

    create layout

---
+++ client/src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SideBar } from "../Sidebar";
+import { Header } from "../Header";
+
+export const getNoneLayout = (page: React.ReactElement) => page;
+
+export const getDefaultLayout = (page: React.ReactElement) => {
+  return (
+    <div className="flex min-h-screen min-w-full">
+      <SideBar />
+      <div className="flex w-full flex-col">
+        <Header />
+        {page}
+      </div>
+    </div>
+  );
+};
