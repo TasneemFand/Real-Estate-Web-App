056ba4b653195f5db1f4af2b2d79d2a3622863db
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-12-05 15:53:54 +0300

    create sidebar

---
+++ client/src/components/Sidebar/index.tsx
@@ -0,0 +1,41 @@
+import { sidebarMenu } from "@/data/sideBarMenu";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import { useTranslation } from "react-i18next";
+import resources from "@/types/resources";
+
+type KeyboardEventKeys = keyof typeof resources.layoutSideBar;
+
+export const SideBar = () => {
+  const { t } = useTranslation("layoutSideBar");
+
+  return (
+    <div className="flex min-h-full w-64 flex-col items-start gap-3 bg-white px-4 py-6 max-sm:hidden">
+      <div
+        id="logo"
+        className="mb-7 flex w-full items-center gap-2 max-sm:hidden"
+      >
+        <img src={logo} alt="logo" />
+        <h1 className="text-2xl font-bold text-primary-foreground">Yariga</h1>
+      </div>
+      {sidebarMenu.map((menu) => (
+        <NavLink
+          key={menu.title}
+          to={menu.path}
+          className={({ isActive, isPending }) =>
+            isActive
+              ? "sideBarLink ActiveLink"
+              : isPending
+              ? "pending"
+              : "sideBarLink hoverLink bg-white text-secondary-foreground "
+          }
+        >
+          {menu.Icon}
+          <span className="text-base font-bold">
+            {t([menu.title as unknown as KeyboardEventKeys])}
+          </span>
+        </NavLink>
+      ))}
+    </div>
+  );
+};
---
+++ client/src/data/sideBarMenu.tsx
@@ -0,0 +1,41 @@
+import {
+  LayoutGrid,
+  CircleUserRound,
+  Building2,
+  Users,
+  Star,
+  MessageSquare,
+} from "lucide-react";
+
+export const sidebarMenu = [
+  {
+    title: "Dashboard",
+    Icon: <LayoutGrid className="h-6 w-6" />,
+    path: "/",
+  },
+  {
+    title: "Property",
+    Icon: <Building2 className="h-6 w-6" />,
+    path: "/property",
+  },
+  {
+    title: "Agent",
+    Icon: <Users className="h-6 w-6" />,
+    path: "/agent",
+  },
+  {
+    title: "Review",
+    Icon: <Star className="h-6 w-6" />,
+    path: "/review",
+  },
+  {
+    title: "Message",
+    Icon: <MessageSquare className="h-6 w-6" />,
+    path: "/message",
+  },
+  {
+    title: "myProfile",
+    Icon: <CircleUserRound className="h-6 w-6" />,
+    path: "/myProfile",
+  },
+];
