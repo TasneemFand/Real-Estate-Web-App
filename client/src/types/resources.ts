914bc00f93561287005dd80df12ea706131851df
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-12-05 15:15:26 +0300

    add react i18n and its config

---
+++ client/src/types/resources.ts
@@ -0,0 +1,11 @@
+import layoutHeader from '../i18n/locales/en/layoutHeader.json';
+import layoutSideBar from '../i18n/locales/en/layoutSideBar.json';
+import notfound from '../i18n/locales/en/notfound.json';
+
+const resources = {
+  layoutHeader,
+  layoutSideBar,
+  notfound
+} as const;
+
+export default resources;
