e7a143de60a007d5094337e2afe6397099bea268
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-12-11 13:56:24 +0300

    add toggle group for filters

--- client/src/pages/home/components/PropertyListFilters.tsx
+++ client/src/pages/home/components/PropertyListFilters.tsx
@@ -1,21 +1,31 @@
-import { Button } from "@/components/ui/button";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { filters } from "@/data/PropertyListFilters";
 import resources from "@/types/resources";
 import { useTranslation } from "react-i18next";
 
 type TranslationKeys = keyof typeof resources.dashboard;
-
-export const PropertyListFilters = () => {
+type TProps = {
+  handleFilter: (value: string) => void;
+};
+export const PropertyListFilters = ({ handleFilter }: TProps) => {
   const { t } = useTranslation("dashboard");
 
   return (
-    <div className="flex flex-wrap gap-3">
+    <ToggleGroup
+      type="single"
+      onValueChange={(value) => handleFilter(value)}
+      defaultValue="popular"
+      className="flex flex-wrap gap-3"
+    >
       {filters.map((filter) => (
-        <Button className=" hoverLink rounded-[6px] bg-hoverColor px-2 py-2 text-xs font-semibold text-secondary-foreground">
-          {" "}
+        <ToggleGroupItem
+          className=" rounded-[6px] px-2 py-2 text-xs font-semibold "
+          value={filter[0].toLowerCase() + filter.slice(1)}
+          variant={"primary"}
+        >
           {t([filter as TranslationKeys])}
-        </Button>
+        </ToggleGroupItem>
       ))}
-    </div>
+    </ToggleGroup>
   );
 };
